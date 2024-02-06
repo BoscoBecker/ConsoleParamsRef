aa7265ed876115bb78b00faa921aa9aa19cf6662
Joao Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-02-05 22:23:24 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleParamsRef.Tripe3;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Calculator Triple = new Calculator();
+        /// Usando ref 
+        int valor = 10;        
+        Triple.Tripe(ref valor);
+        Console.WriteLine(valor);
+
+        /// Usando out
+        int valor2 = 10;
+        int triple;
+        Triple.Tripe(valor2, out triple);
+        Console.WriteLine(triple); 
+
+
+    }
+}
---
+++ Tripe/Calculator.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleParamsRef.Tripe3
+{
+    public class Calculator
+    {
+        public void Tripe(ref int x)
+        {
+            x = x * 3;
+        }
+
+        public void Tripe(int origin, out int result)
+        {
+            result = origin * 3;
+
+        }
+    }
+}
